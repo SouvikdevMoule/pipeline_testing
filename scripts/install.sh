bbed63b219ca812c839edb0fd600b29504dc35af
SouvikdevMoule <souvikdev2002@gmail.com>
2025-07-11 19:39:18 +0530

    CICD UPdated

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
-echo "Restarting Apache Web Server"
-sudo systemctl restart httpd || sudo systemctl restart nginx
+
+echo "Setting permissions for /var/www/html"
+sudo chmod -R 755 /var/www/html
+sudo chown -R www-data:www-data /var/www/html
+
+echo "Restarting web server..."
+sudo systemctl restart apache2 || sudo systemctl restart nginx
