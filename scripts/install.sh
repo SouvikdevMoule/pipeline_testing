64537b351e6a64574ce8570e017016a6f352cb10
SouvikDev2002 <souvikdev2002@gmail.com>
2025-07-12 22:14:39 +0530

    Delete scripts directory

--- scripts/install.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-echo "Setting permissions for /var/www/html"
-sudo chmod -R 755 /var/www/html
-sudo chown -R www-data:www-data /var/www/html
-
-echo "Restarting web server..."
-sudo systemctl restart apache2 || sudo systemctl restart nginx
